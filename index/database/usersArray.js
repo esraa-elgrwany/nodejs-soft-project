8786d838faf424fc5829c0699363b09266ef2b95
NourhanSami <117237957+NourhanSami@users.noreply.github.com>
2023-05-24 21:43:32 +0300

    Create usersArray.js

---
+++ index/database/usersArray.js
@@ -0,0 +1,36 @@
+export const doctors = [
+    {
+        id: 1,
+        name: "Dr/ Ahmed",
+        subject1: "JAVA",
+        subject2: "ANDROID",
+        subject3:"SOFT1",
+        subject4:"SOFT2"
+    },
+    {
+        id: 2,
+        name: "Dr/ Mohammed",
+        subject1: "C++",
+        subject2: "MATH2",
+        subject3:"AI",
+        subject4:"ML"
+    },
+    {
+        id: 3,
+        name: "Dr/ Sara",
+        subject1: "SECURITY",
+        subject2: "NETWORK",
+        subject3:"OS1",
+        subject4:"OS2"
+    },
+    {
+        id: 4,
+        name: "Dr/ Heba",
+        subject1: "LOGIC",
+        subject2: "MEDIA",
+        subject3:"IMAGE",
+        subject4:"GRAPHIC"
+    }
+
+]
+
