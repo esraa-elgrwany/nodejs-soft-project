a89ff118ff7dac4872daf55dd860aba927246ed2
NENO❤️ <you@example.com>
2023-05-24 10:49:09 +0300

    Add Doctor

---
+++ index/database/subject.js
@@ -0,0 +1,36 @@
+import { Schema,model } from "mongoose";
+import doctor from "./doctor.js";
+const subject = new Schema({
+   
+    name:{
+        type :String,
+        required:true,
+    },
+    code:{
+        type :String,
+        required:false,
+    },
+  
+    department:{
+        type : Schema.Types.ObjectId,
+        required:false,
+        ref:"department",
+    },
+    requirements:{
+        type :String,
+        required:false,
+    
+    },
+    stname:{
+        type :String,
+        required:true,
+    },
+    doctor:{
+        type : Schema.Types.ObjectId,
+        required:false,
+        ref:"doctor",
+    },
+},
+{timestamps:true});
+ 
+export default model("subject",subject);
