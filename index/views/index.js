955145bc3c8e5c1e677b19e91f0cd829b58bf6e3
NourhanSami <117237957+NourhanSami@users.noreply.github.com>
2023-05-24 21:49:25 +0300

    Create index.js

---
+++ index/views/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { engine } from 'express-handlebars';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import subjectsRouter from "./routes/subjectsRouter.js";
+import departmentRouter from "./routes/departmentRouter.js";
+import methodOverride from "method-override";
+import { doctors } from "../../../../Desktop/PROJECTSOFT/p6/database/usersArray.js";
+import doctorRoute from './routes/doctorRoute.js'
+
+dotenv.config();
+
+const mongoconnectionUrl = "mongodb://127.0.0.1:27017/project";
+mongoose.connect(process.env.mongoconnectionUrl);
+
+const app = express();
+app.use(express.urlencoded({extended:true}))
+const port =5000;
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(methodOverride('_method'));
+app.use("/subjects",subjectsRouter);
+app.use("/departments",departmentRouter);
+app.use("/doctor", doctorRoute)
+
+app.listen(process.env.port,() =>{
+console.log("started the application on http://localhost:" + port );
+});
+
+
